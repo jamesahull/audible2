6da9a0318a191a4f592fe0f6e45bd1c8e5588c38
James Hull <james.a.hull@gmail.com>
2014-08-08 14:35:02 +0100

    SQL tables

--- createTables.sql
+++ createTables.sql
@@ -24,3 +24,50 @@ INSERT INTO northern_aliweb.poststatus
 (status,name) VALUES ('Y', 'Approved');
 INSERT INTO northern_aliweb.poststatus
 (status,name) VALUES ('N', 'To be removed');
+
+
+CREATE TABLE `feedbackreqds` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `posts` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `content` mediumtext,
+  `userid` int(11) DEFAULT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `showpost` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+CREATE TABLE `questions` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `question` mediumtext,
+  `answer` mediumtext,
+  `answered` char(1) DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `active` char(1) DEFAULT NULL,
+  `age` tinyint(4) DEFAULT NULL,
+  `sex` char(1) DEFAULT NULL,
+  `mhp` char(1) DEFAULT NULL,
+  `mentalhealth` varchar(255) DEFAULT NULL,
+  `ethnic` tinyint(4) DEFAULT NULL,
+  `ethnicgroup` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `usedirect` char(1) DEFAULT NULL,
+  `sector` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+
